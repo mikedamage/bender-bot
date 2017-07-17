bfb8a545602a0f21a7db9da7c0094003c0ce68d9
Mike Green <mike.is.green@gmail.com>
2017-07-17 15:24:57 -0400

    Migrate scratchpad code to bender.js module class

--- scratch.js
+++ scratch.js
@@ -1,8 +1,11 @@
+global.Olm = require('olm');
+const replify = require('replify');
 const bunyan = require('bunyan');
 const sdk = require('matrix-js-sdk');
 const { userId, accessToken, baseUrl } = require('./testaccount.json');
 
-const logger = bunyan.createLogger({ app: 'bender-scratch' });
+const logger = bunyan.createLogger({ name: 'bender-scratch' });
+logger.info('Start Matrix client');
 
 const client = sdk.createClient({
   baseUrl,
@@ -18,3 +21,17 @@ client.on('RoomMember.membership', (evt, member) => {
   logger.info({ member, evt }, 'Received room invitation');
   client.joinRoom(member.roomId).then(() => logger.info({ roomId: member.roomId }, 'Joined room'));
 });
+
+// Log messages received in joined rooms
+client.on('Room.timeline', (evt, room, toStartOfTimeline) => {
+  if (toStartOfTimeline || evt.getType() !== 'm.room.message') return;
+
+  const sender = evt.getSender();
+  const content = evt.getContent();
+
+  logger.info({ room, sender, content }, `Message received in ${room.name}`);
+});
+
+replify('bender', client);
+
+client.startClient();
