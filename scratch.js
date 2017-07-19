388b36c5d400ea20b210e00d4aaf9123f24cb2c4
Mike Green <mike.is.green@gmail.com>
2017-07-19 11:23:49 -0400

    Add Avatar plugin, for automatically setting user profile images

--- scratch.js
+++ scratch.js
@@ -1,15 +1,18 @@
 const Bender = require('./lib/bender');
 const AcceptInvitations = require('./lib/plugins/accept-invitations');
 const NormalizeMessages = require('./lib/plugins/normalize-messages');
+const Avatar = require('./lib/plugins/avatar');
 const { userId, accessToken, baseUrl, deviceId } = require('./testaccount.json');
 
 const bender = new Bender({ userId, accessToken, baseUrl, deviceId, repl: true, logging: { level: 'info' } });
 
+const avatar = new Avatar(bender, { avatarFile: './avatar.jpg' });
 const acceptInvitations = new AcceptInvitations(bender);
 const normalizeMessages = new NormalizeMessages(bender);
 
 bender.logger.info(acceptInvitations.rules);
 
+bender.use(avatar);
 bender.use(acceptInvitations);
 bender.use(normalizeMessages);
 
