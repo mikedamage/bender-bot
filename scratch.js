4213f1021192e0d51f27653658305727ef8e8fb3
Mike Green <mike.is.green@gmail.com>
2017-07-18 22:10:11 -0400

    Define basic pattern for plugin creation and registration, but rules engine conditions are not parsing or executing properly yet

--- scratch.js
+++ scratch.js
@@ -1,37 +1,18 @@
-global.Olm = require('olm');
-const replify = require('replify');
-const bunyan = require('bunyan');
-const sdk = require('matrix-js-sdk');
+const Bender = require('./lib/bender');
+const AcceptInvitations = require('./lib/plugins/accept-invitations');
+const NormalizeMessages = require('./lib/plugins/normalize-messages');
 const { userId, accessToken, baseUrl } = require('./testaccount.json');
 
-const logger = bunyan.createLogger({ name: 'bender-scratch' });
-logger.info('Start Matrix client');
+const bender = new Bender({ userId, accessToken, baseUrl, repl: true, logging: { level: 'info' } });
 
-const client = sdk.createClient({
-  baseUrl,
-  userId,
-  accessToken,
-});
+const acceptInvitations = new AcceptInvitations(bender);
+const normalizeMessages = new NormalizeMessages(bender);
 
-logger.debug({ client }, 'Create Matrix JS SDK client instance');
+bender.logger.info(acceptInvitations.rules);
 
-// Auto-join rooms when invited
-client.on('RoomMember.membership', (evt, member) => {
-  if (!(member.membership === 'invite' && member.userId === userId)) return;
-  logger.info({ member, evt }, 'Received room invitation');
-  client.joinRoom(member.roomId).then(() => logger.info({ roomId: member.roomId }, 'Joined room'));
-});
+bender.use(acceptInvitations);
+bender.use(normalizeMessages);
 
-// Log messages received in joined rooms
-client.on('Room.timeline', (evt, room, toStartOfTimeline) => {
-  if (toStartOfTimeline || evt.getType() !== 'm.room.message') return;
+bender.on('connect', () => bender.logger.info('Connected to server'));
 
-  const sender = evt.getSender();
-  const content = evt.getContent();
-
-  logger.info({ room, sender, content }, `Message received in ${room.name}`);
-});
-
-replify('bender', client);
-
-client.startClient();
+bender.connect();
